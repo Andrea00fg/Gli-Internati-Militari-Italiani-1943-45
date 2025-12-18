02f80649878f114c2862d256ee07c461424f3a22
Andrea00fg <andreaperrucci09@gmail.com>
2025-12-19 00:46:42 +0100

    Update Luoghi.js

--- javascript/Luoghi.js
+++ javascript/Luoghi.js
@@ -1,6 +1,3 @@
-<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
   const map = L.map('map').setView([51.5, 10.5], 5);
