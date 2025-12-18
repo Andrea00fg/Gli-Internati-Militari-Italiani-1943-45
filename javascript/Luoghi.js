f6a16bcfb47de8751015ea642dc00ebf0a4dde65
Andrea00fg <andreaperrucci09@gmail.com>
2025-12-19 00:51:10 +0100

    Update Luoghi.js

--- javascript/Luoghi.js
+++ javascript/Luoghi.js
@@ -1,18 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
 
-<script>
   const map = L.map('map').setView([51.5, 10.5], 5);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    attribution: 'Map data © OpenStreetMap contributors'
   }).addTo(map);
 
   const luoghi = [
-    { nome: "•Fichtenhain", coords: [51.345, 6.521] },
-    { nome: "▸Kassel", coords: [51.3127, 9.4797] },
-    { nome: "•■Deblin", coords: [51.5562, 21.8547] },
-    { nome: "•Poznan", coords: [52.4064, 16.9252] },
-    { nome: "•Hohenstein", coords: [53.7761, 20.9825] },
-    { nome: "🔺Dora - Nordhausen", coords: [51.5025, 10.7764] }
+    { nome: "• Fichtenhain", coords: [51.345, 6.521] },
+    { nome: "▸ Kassel", coords: [51.3127, 9.4797] },
+    { nome: "•■ Deblin", coords: [51.5562, 21.8547] },
+    { nome: "• Poznan", coords: [52.4064, 16.9252] },
+    { nome: "• Hohenstein", coords: [53.7761, 20.9825] },
+    { nome: "🔺 Dora - Nordhausen", coords: [51.5025, 10.7764] }
   ];
 
   luoghi.forEach(l => {
@@ -21,14 +21,18 @@
   });
 
   const legenda = L.control({ position: 'bottomright' });
-  legenda.onAdd = function(map) {
+
+  legenda.onAdd = function () {
     const div = L.DomUtil.create('div', 'legend');
-    div.innerHTML += "<strong>Legenda</strong><br>";
-    div.innerHTML += "• Campi di prigionia per sottoufficiali e militari<br>";
-    div.innerHTML += "■ Campi di prigionia per ufficiali<br>";
-    div.innerHTML += "🔺Campi di concentramento<br>";
-    div.innerHTML += "▸ Sede dei comandanti dei prigionieri di guerra<br>";
+    div.innerHTML = `
+      <strong>Legenda</strong><br>
+      • Campi di prigionia per sottoufficiali e militari<br>
+      ■ Campi di prigionia per ufficiali<br>
+      🔺 Campi di concentramento<br>
+      ▸ Sede dei comandanti dei prigionieri di guerra
+    `;
     return div;
   };
+
   legenda.addTo(map);
-</script>
+});
